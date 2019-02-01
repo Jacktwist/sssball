25725f0ee774a9525b4357990f213921351a786d
Jacktwist <william.doring@gmail.com>
2019-02-01 00:26:24 -0600

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -183,6 +183,7 @@ impl Object {
             }
         }
     }
+
 }
 
 // scales inches to ascii blocks for drawing
@@ -212,6 +213,7 @@ fn make_map() -> Map {
         map[0][y as usize] = Tile::wall();
         map[(MAP_WIDTH-1) as usize][y as usize] = Tile::wall();
     }
+    map[(MAP_WIDTH-1) as usize][(MAP_HEIGHT-1) as usize] = Tile::wall();
     //fill field boundary
     let mut x1 = (MAP_WIDTH/2) - (FIELD_WIDTH/2 + FIELD_BORDER_WIDTH)/SQUARE_SCALE;
     let mut y1 = (MAP_HEIGHT/2) - (FIELD_HEIGHT/2 + FIELD_BORDER_WIDTH)/SQUARE_SCALE;
